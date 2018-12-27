cbbff410ea5f9c72e9589611c2977651dace5478
mathuranchal90 <Anchal.Mathur@BOLD-ANCHALM>
2018-12-27 12:06:19 +0530

    File Upload-Service added

--- upload-service/src/main/java/com/sms/uploadservice/UploadServiceApplication.java
+++ upload-service/src/main/java/com/sms/uploadservice/UploadServiceApplication.java
@@ -1,30 +1,26 @@
 package com.sms.uploadservice;
 
-import java.util.TimeZone;
 
-import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import com.sms.uploadservice.properties.FileStorageProperties;
+
+
+
 
 
 @SpringBootApplication
 @EnableEurekaClient
-@EntityScan(basePackageClasses = { 
-		UploadServiceApplication.class,
-		Jsr310JpaConverters.class 
-})
+@EnableConfigurationProperties({FileStorageProperties.class})
 public class UploadServiceApplication {
 	
-	@PostConstruct
-	void init() {
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(UploadServiceApplication.class, args);
--- upload-service/src/main/java/com/sms/uploadservice/controller/UploadController.java
+++ upload-service/src/main/java/com/sms/uploadservice/controller/UploadController.java
@@ -1,17 +1,29 @@
 package com.sms.uploadservice.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.sms.uploadservice.payload.UploadFileResponse;
+import com.sms.uploadservice.service.FileStorageService;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestController
 public class UploadController {
